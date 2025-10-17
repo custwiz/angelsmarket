56820a134c137ab6d145edc9b44ea7b9ac4742db
TheMindCollective <software@gauravdembla.com>
2025-10-17 18:06:06 +0530

    update

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,5 +1,15 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+// import { createRoot } from 'react-dom/client'
+// import App from './App.tsx'
+// import './index.css'
 
-createRoot(document.getElementById("root")!).render(<App />);
+// createRoot(document.getElementById("root")!).render(<App />);
+
+
+
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
+import axios from "axios";
+// axios.defaults.baseURL = "http://localhost:4000";
+axios.defaults.baseURL = "https://shop.angelsonearthhub.com";
+createRoot(document.getElementById("root")!).render(<App />);
